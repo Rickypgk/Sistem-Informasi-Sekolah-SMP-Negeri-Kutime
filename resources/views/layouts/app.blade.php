db05035e25ce82666c2533b42957902f0c7dcf50
Ricky <rickypgk3@gmail.com>
2026-04-20 21:23:58 +0700

    Updates: 2026-04-20T14:23:55.935Z

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1362,7 +1362,7 @@
                          'icon'=>'M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z'],
                         ['route'=>'guru.wali-kelas',          'label'=>'Wali Kelas',        'match'=>'guru.wali-kelas*',
                          'icon'=>'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z'],
-                        ['route'=>'guru.jadwal-mengajar',     'label'=>'Jadwal Mengajar',   'match'=>'guru.jadwal-mengajar*',
+                        ['route'=>'guru.jadwal-mengajar.index',     'label'=>'Jadwal Mengajar',   'match'=>'guru.jadwal-mengajar*',
                          'icon'=>'M8 7V3m8 4V3M5 11h14M5 19h14M5 5h2m10 0h2'],
                         ['route'=>'guru.absensi-siswa.index', 'label'=>'Absensi Siswa',     'match'=>'guru.absensi-siswa*',
                          'icon'=>'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4'],
