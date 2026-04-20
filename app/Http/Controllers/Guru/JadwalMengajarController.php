15d78856c93c2c25cfc43cab11481d276f14d797
Ricky <rickypgk3@gmail.com>
2026-04-20 20:36:09 +0700

    Updates: 2026-04-20T13:36:07.423Z

--- app/Http/Controllers/Guru/JadwalMengajarController.php
+++ app/Http/Controllers/Guru/JadwalMengajarController.php
@@ -73,6 +73,10 @@ class JadwalMengajarController extends Controller
             'academic_year'    => ['required', 'regex:/^\d{4}\/\d{4}$/'],
             'semester'         => 'required|in:1,2',
             'notes'            => 'nullable|string|max:500',
+            'name'        => 'required|string|max:100',
+            'code'        => 'required|string|max:20',
+            'color'       => 'nullable|string|max:7',
+            'description' => 'nullable|string|max:200',
         ]);
 
         Timetable::create(array_merge($validated, [
